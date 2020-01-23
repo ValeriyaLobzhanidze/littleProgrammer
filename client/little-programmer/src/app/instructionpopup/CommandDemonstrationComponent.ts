b577c981b86cdc7c52df21544e4fb0e1911b79ea
lobzhanidze <lera.lobzhanidze@gmail.com>
2020-01-23 14:21:57 +0300

    ddd

--- client/little-programmer/src/app/instructionpopup/CommandDemonstrationComponent.ts
+++ client/little-programmer/src/app/instructionpopup/CommandDemonstrationComponent.ts
@@ -5,61 +5,81 @@ export default class CommandDemonstrationComponent implements ComponentI {
 
   private ballX = 0;
   private ballY = 0;
-  private currentUpdateBallFunc: () => void;
-  private initFlag = false;
+  private ballColor = "#58fbce";
+  private currentUpdateBallFunc: () => { x: number, y: number };
+  private currentInstruction;
+  private instrIdx = 0;
+
+  private currentYOfParabola = 0;
 
   private readonly instructions: Instruction[];
 
   constructor(instructions: Instruction[]) {
     this.instructions = instructions;
+    this.initInstructions();
+    this.currentUpdateBallFunc = this.positiveDirection;
+    this.currentInstruction = this.instructions[this.instrIdx];
+  }
+
+  private initInstructions() {
+    let x = 40;
+    let y = 60;
+    for (let instruction of this.instructions) {
+      instruction.x = x;
+      instruction.y = y;
+      y += 50;
+    }
   }
 
   private renderInstructions(canvas: any) {
     let ctx = canvas.getContext('2d');
     ctx.font = "30px KBSticktoIt";
 
-    let y = 60;
     for (let instruction of this.instructions) {
-      ctx.fillStyle = instruction.getColor();
-      ctx.fillText(instruction.getInstruction(), 40, y);
-      y += 50;
+      ctx.fillStyle = instruction.color;
+      ctx.fillText(instruction.instruction, instruction.x, instruction.y);
     }
   }
 
   private renderBall(canvas: any) {
     let ctx = canvas.getContext('2d');
+    ctx.fillStyle = this.ballColor;
     ctx.beginPath();
-    ctx.arc(this.ballX, this.ballY, 15, 0, 2 * Math.PI);
+    ctx.arc(this.ballX, this.ballY, 10, 0, 2 * Math.PI);
     ctx.fill();
   }
 
   private update() {
-    if (!this.initFlag) {
-      this.currentUpdateBallFunc = this.negativeDirection;
-      this.initFlag = true;
-    }
-    if (this.ballX < 0) {
+    let testUpdateResult = this.currentUpdateBallFunc();
+    if (testUpdateResult.x < 0) {
       this.ballX = 0;
       this.currentUpdateBallFunc = this.positiveDirection;
+    } else if (testUpdateResult.x >= this.currentInstruction.x) {
+      this.currentUpdateBallFunc = this.negativeDirection;
+      this.currentInstruction = this.instructions[++this.instrIdx];
+      this.currentYOfParabola += Math.sqrt(this.ballX) * 10;
     }
-    this.currentUpdateBallFunc();
+
+    let updateResult = this.currentUpdateBallFunc();
+    this.ballX = updateResult.x;
+    this.ballY = updateResult.y;
   }
 
-  private positiveDirection() {
-    this.ballX += 0.2;
-    this.ballY = Math.sqrt(this.ballX) * 5;
+  private positiveDirection(): { x: number, y: number } {
+    let sqrt = this.currentYOfParabola + Math.sqrt(this.ballX) * 5;
+    return {x: this.ballX + 0.2, y: sqrt};
   }
 
-  private negativeDirection() {
-    this.ballX -= 0.2;
-    this.ballY = -Math.sqrt(this.ballX) * 5;
+  private negativeDirection(): { x: number, y: number } {
+    let func = this.currentYOfParabola - Math.sqrt(this.ballX) * 5;
+    return {x: this.ballX - 0.2, y: func};
   }
 
   render(canvas: any) {
     let ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.update();
     this.renderInstructions(canvas);
     this.renderBall(canvas);
+    this.update();
   }
 }
--- client/little-programmer/src/app/instructionpopup/Instruction.ts
+++ client/little-programmer/src/app/instructionpopup/Instruction.ts
@@ -1,21 +1,42 @@
 export default class Instruction {
-  private instruction: string;
-  private color: string = "pink";
-  private x: number = 0;
-  private y: number = 0;
+  private _instruction: string;
+  private _color: string = "rgba(187, 116, 251, 0.83)";
+  private _x: number = 0;
+  private _y: number = 0;
 
-  constructor(instruction: string, color?: string, x?: number, y?: number) {
-    this.instruction = instruction;
-    this.color = color;
-    this.x = x;
-    this.y = y;
+  constructor(instruction: string) {
+    this._instruction = instruction;
   }
 
-  public getInstruction(): string {
-    return this.instruction;
+  get instruction(): string {
+    return this._instruction;
   }
 
-  public getColor(): string {
-    return this.color;
+  set instruction(value: string) {
+    this._instruction = value;
+  }
+
+  get color(): string {
+    return this._color;
+  }
+
+  set color(value: string) {
+    this._color = value;
+  }
+
+  get x(): number {
+    return this._x;
+  }
+
+  set x(value: number) {
+    this._x = value;
+  }
+
+  get y(): number {
+    return this._y;
+  }
+
+  set y(value: number) {
+    this._y = value;
   }
 }
