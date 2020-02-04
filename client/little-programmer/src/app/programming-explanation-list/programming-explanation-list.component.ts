5e65311f66377716dc9dee1fb0d21c9048ecd69d
lobzhanidze <lera.lobzhanidze@gmail.com>
2020-02-04 18:35:15 +0300

    ddd

--- client/little-programmer/src/app/programming-explanation-list/programming-explanation-list.component.ts
+++ client/little-programmer/src/app/programming-explanation-list/programming-explanation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-programming-explanation-list',
@@ -9,7 +9,8 @@ export class ProgrammingExplanationListComponent implements OnInit {
 
   private explanationList: ProgrammingExplanationListComponent[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
